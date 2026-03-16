d5eb655f605b94b45e8664c05ecca3a07dbdebb6
NEGROY <nery.diaz96@gmail.com>
2026-03-16 10:09:16 -0600

    FUNCION DE 6 MESES ULTIMOS

---
+++ app/service/funciones.py
@@ -0,0 +1,38 @@
+## FUNCION PARA REALZIAR 
+import calendar
+from datetime import datetime
+
+## FUNCION PARA REALIZAR LOS CALCULOS DE LOS ULTMOS 6 MEESE
+def ultimos_6_meses(mes, anio):
+
+    meses_es = [
+        "ENE","FEB","MAR","ABR","MAY","JUN",
+        "JUL","AGO","SEP","OCT","NOV","DIC"
+    ]
+
+    data = {}
+
+    for i in range(6):
+        m = mes - i
+        y = anio
+
+        if m <= 0:
+            m += 12
+            y -= 1
+
+        mes_num = f"{m:02d}"
+        ultimo_dia = calendar.monthrange(y, m)[1]
+        fecha_ini = f"01{mes_num}{y}"
+        fecha_fin = f"{ultimo_dia:02d}{mes_num}{y}"
+        nombre_mes = f"{meses_es[m-1]}-{str(y)[2:]}"
+        suf = "" if i == 0 else f"_{i}"
+
+        data[f"MESN{suf}"] = mes_num
+        data[f"MES{suf}"] = nombre_mes
+        data[f"FECHA_INI{suf}"] = fecha_ini
+        data[f"FECHA_FIN{suf}"] = fecha_fin
+
+    data["ANO_1"] = anio if mes > 5 else anio - 1
+    data["ANO_2"] = anio
+
+    return data
--- app/views/template_reporte/prueba.py
+++ app/views/template_reporte/prueba.py
@@ -1,35 +1,69 @@
 from django.shortcuts import render
 from app.service.conexion import obtener_conexion
 from app.service.reportes_sql import historico_de_indicadores
-# datos del modelo 
+from app.service.funciones import ultimos_6_meses
 from app.models.empresas_catalogo import EmpresasCatalogo
 
-
+# http://127.0.0.1:8000/reporte-prueba/?empresa=3&mes=03&anio=2026&total_enlaces=55
 def reporte_prueba(request):
+
+    # PARAMETROS
+    id_empresa = request.GET.get("empresa")
+    mes = request.GET.get("mes")        # MARZO, ENERO, FEBREO
+    anio = request.GET.get("anio")
+    total_enlaces = request.GET.get("total_enlaces")
+
+    # convertir 2026-01-01 -> 01012026 fechaINI = fechaINI.replace("-", "")
+
+    empresa = None
+    fechaINI = '0102' + anio
+
+    if id_empresa:
+        empresa = EmpresasCatalogo.objects.get(COD=id_empresa)
+
+        # VALIDAR SI CAMBIO TOTAL ENLACES
+        if total_enlaces:
+            total_enlaces = int(total_enlaces)
+
+            if empresa.tt_sitioa != total_enlaces:
+                empresa.tt_sitioa = total_enlaces
+                empresa.save(update_fields=["tt_sitioa"])
+
     # CONSULTA ORACLE
     conn = obtener_conexion()
     cursor = conn.cursor()
-    sql = historico_de_indicadores()
-    ##sql = historico_de_indicadores(empresa.parque_where)
+
+    # REALZIA CONSULTA DE LOS DATOS DE historico_de_indicadores  
+    if empresa:
+        sql = historico_de_indicadores(empresa.parque_where)
+        # sql = historico_de_indicadores()
+    else:
+        sql = historico_de_indicadores()
+ 
     cursor.execute(sql)
-
     columnas = [col[0] for col in cursor.description]
-
     resultados = []
     for fila in cursor.fetchall():
         resultados.append(dict(zip(columnas, fila)))
-
-    cursor.close()
+    #CIERRA LAS CONEXIONES cursor.close() conn.close()
+    cursor.close() 
     conn.close()
 
-    # CONSULTA DJANGO ORM
-    empresas = EmpresasCatalogo.objects.filter(COD=2).order_by("ORDEN")
+    meses = ultimos_6_meses(int(mes), int(anio))
+
 
     return render(
         request,
         "template_reporte/preuba.html",
         {
             "data": resultados,
-            "empresas": empresas
+            "meses": meses,
+            "sql": sql,
+            "id": id_empresa,
+            "empresa": empresa,
+            "mes": mes,
+            "anio": anio
         }
     )
+
+#http://127.0.0.1:8000/reporte-prueba/?empresa=3&mes=MARZO&anio=2026&total_enlaces=55
